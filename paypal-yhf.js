29a74fc3a077af63535619c8713c4759d6e9cd5e
Robin Wang <robinwang@Robins-MBP.fios-router.home>
2016-12-24 17:13:29 -0500

    2016.2

---
+++ paypal-yhf.js
@@ -0,0 +1,9 @@
+document.write('<FORM ACTION="https://www.paypal.com/cgi-bin/webscr" METHOD="post" TARGET="_top">\
+				<INPUT TYPE="hidden" NAME="cmd" VALUE="_s-xclick">\
+				<INPUT TYPE="hidden" NAME="hosted_button_id" VALUE="PNVWFJG59TRP4">\
+				<INPUT TYPE="image"	\
+				 SRC="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" BORDER="0"	\
+				 NAME="submit" ALT="PayPal - The safer, easier way to pay online!">	\
+				<IMG ALT="" BORDER="0" \
+				 SRC="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" WIDTH="1" HEIGHT="1">\
+				</FORM> ');
